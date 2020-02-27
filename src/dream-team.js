802ce3c96283f5abb7cff6f894ceec6fe2e1d2f0
IAleks94 <a12344@mail.ru>
2020-02-27 14:55:36 +0300

    finisf dreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,24 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arrNames) {
+  if (Array.isArray(arrNames)) {
+    let teamName = [];
+    arrNames.forEach(element => {
+      if (typeof element === "string") {
+        let UpperLetter = element.trim().match(/^[A-Z]/i);
+        if (UpperLetter) {
+          teamName.push(UpperLetter[0].toUpperCase());
+        }
+      }
+    });
+
+    if (teamName.length) {
+      return teamName
+        .sort((a, b) => {
+          if (a > b) return 1;
+          if (a == b) return 0;
+          if (a < b) return -1;
+        })
+        .join("");
+    }
+  }
+  return false;
 };
