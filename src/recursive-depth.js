5d50bbf23c2374416f31308724d808311e240d4d
IAleks94 <a12344@mail.ru>
2020-02-28 15:25:53 +0300

    delit return

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,25 +1,25 @@
-module.exports = class DepthCalculator {
+class DepthCalculator {
   calculateDepth(arr) {
     let depth = 1;
     if (arr.length) {
       arr.forEach(element => {
         if (Array.isArray(element)) {
-          return depth = Math.max(1 + this.calculateDepth(element), depth);
+        depth = Math.max(1 + this.calculateDepth(element), depth);
         } 
       });
     }
     return depth;
   }
-};
+}
 
 
-//  const instance = new DepthCalculator();
-//   const calculateDepth = instance.calculateDepth.bind(instance);
+ const instance = new DepthCalculator();
+  const calculateDepth = instance.calculateDepth.bind(instance);
 
-//   console.log(calculateDepth([1, 2, 3, 4, 5, [7]]));
-//   console.log(calculateDepth([1, 2, 3, [[1]], 4, 5, [1]]));
-//   console.log(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]));
-//   console.log(calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]));
+  console.log(calculateDepth([1, 2, 3, 4, 5, [7]]));
+  console.log(calculateDepth([1, 2, 3, [[1]], 4, 5, [1]]));
+  console.log(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]));
+  console.log(calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]));
 
 // console.log(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]));
 
