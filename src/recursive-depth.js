bf29c871ca8c383377e3ebc648b54c8f12714450
IAleks94 <a12344@mail.ru>
2020-02-28 15:19:38 +0300

    finish recursive

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,29 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  calculateDepth(arr) {
+    let depth = 1;
+    if (arr.length) {
+      arr.forEach(element => {
+        if (Array.isArray(element)) {
+          return depth = Math.max(1 + this.calculateDepth(element), depth);
+        } 
+      });
     }
+    return depth;
+  }
 };
+
+
+//  const instance = new DepthCalculator();
+//   const calculateDepth = instance.calculateDepth.bind(instance);
+
+//   console.log(calculateDepth([1, 2, 3, 4, 5, [7]]));
+//   console.log(calculateDepth([1, 2, 3, [[1]], 4, 5, [1]]));
+//   console.log(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]));
+//   console.log(calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]));
+
+// console.log(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]));
+
+// assert.equal(calculateDepth([1, 2, 3, 4, 5, [1]]), 2);
+// assert.equal(calculateDepth([1, 2, 3, [1], 4, 5, [1]]), 2);
+//assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 5);
+//calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 15)
