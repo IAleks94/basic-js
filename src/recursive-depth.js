7f2dd55a3730aca11de3cdd736402615bcd659a8
IAleks94 <a12344@mail.ru>
2020-02-28 16:23:53 +0300

    delit treh

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,4 +1,4 @@
-class DepthCalculator {
+module.exports = class DepthCalculator {
   calculateDepth(arr) {
     let depth = 1;
     if (arr.length) {
@@ -11,19 +11,3 @@ class DepthCalculator {
     return depth;
   }
 }
-
-
- const instance = new DepthCalculator();
-  const calculateDepth = instance.calculateDepth.bind(instance);
-
-  console.log(calculateDepth([1, 2, 3, 4, 5, [7]]));
-  console.log(calculateDepth([1, 2, 3, [[1]], 4, 5, [1]]));
-  console.log(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]));
-  console.log(calculateDepth([1, [8, [[]]], [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]], 2, 3, [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]], [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]));
-
-// console.log(calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]));
-
-// assert.equal(calculateDepth([1, 2, 3, 4, 5, [1]]), 2);
-// assert.equal(calculateDepth([1, 2, 3, [1], 4, 5, [1]]), 2);
-//assert.equal(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 5);
-//calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575',['adas', ['dfg', [0]]]]]), 15)
