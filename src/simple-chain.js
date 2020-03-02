d68a4d05ea86b681f1bcfc9b400a912ce6a4081e
IAleks94 <a12344@mail.ru>
2020-03-02 10:14:19 +0300

    add T in this

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,12 +5,11 @@ const chainMaker = {
   },
   addLink(value) {
     this.linkArr.push(`( ${value} )`);
-    //console.log(this.linkArr);
     return this;
   },
   removeLink(position) {
     if (this.linkArr[position - 1] === undefined || position >= this.linkArr.length || position % 1 != 0) {
-      his.linkArr = [];
+      this.linkArr = [];
       throw new Error("Неправильная позицыя");
     }
     this.linkArr.splice(position - 1, 1);
@@ -29,22 +28,3 @@ const chainMaker = {
 
 module.exports = chainMaker;
 
-//() => chainMaker.addLink(1).addLink(2).addLink(3).removeLink(4)
-//console.log(chainMaker.addLink(1).addLink(2).addLink(3).removeLink(4));
-
-//assert.deepEqual(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain(), '( null )~~( GHI )~~( 333 )~~( 0 )~~( GHI )');
-
-// //console.log(
-//   chainMaker
-//   .addLink("GHI")
-//   .addLink(null)
-//   .reverseChain()
-//   .addLink(333)
-//   .reverseChain()
-//   .reverseChain()
-//   .addLink(0)
-//   .reverseChain()
-//   .reverseChain()
-//   .addLink("GHI")
-//   .finishChain()
-// );
